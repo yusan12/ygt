3050aee0fa4001b7dfe40ead946561043be30276
Y S <haraheri7212@yahoo.co.jp>
2023-01-11 10:47:24 +0900

    make urls.py and chenge settings.py

--- ygt/urls.py
+++ ygt/urls.py
@@ -13,10 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path,include
+
+from django.urls import path
+import ygt.views as ygt
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('ygt/', include('ygt.urls')),
+    path('ygt/', ygt.index),
 ]
